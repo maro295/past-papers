2f8d7aa2dda2c3ad4fd405b08e343c9b16851250
maro295 <80099760+maro295@users.noreply.github.com>
2024-05-08 13:59:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-// Get all links within your subject page (update the selector if needed)
-const links = document.querySelectorAll('.subject-links a'); 
-
-// Add click listeners to all the links
-links.forEach(link => {
-    link.addEventListener('click', function(event) {
-        event.preventDefault(); // Prevent default link behavior (opening new tab)
-
-        const miniBrowser = document.getElementById('miniBrowser');
-        miniBrowser.src = this.href;  // Set the iframe 'src' to the clicked link's URL 
-    });
-});
+function openSubject(subject) {
+  var webViewer = document.getElementById('webViewer');
+  switch (subject) {
+    case 'physics':
+      webViewer.innerHTML = '<iframe src="https://drive.google.com/embeddedfolderview?id=1V_fIJK1101iSlak7EEiBSKCM6g88pnTQ#grid" style="width: 100%; height: 500px; border: none;"></iframe>';
+      break;
+    case 'math':
+      webViewer.innerHTML = '<iframe src="https://drive.google.com/embeddedfolderview?id=1f-hQujxLi2Yb7GrqLVc3i9lKt8tp8jNC#grid" style="width: 100%; height: 500px; border: none;"></iframe>';
+      break;
+    case 'chemistry':
+      webViewer.innerHTML = '<iframe src="https://drive.google.com/embeddedfolderview?id=1U5nAcv5UXaPT0G3rDdp6IbLbq9oR8Lhd#grid" style="width: 100%; height: 500px; border: none;"></iframe>';
+      break;
+    default:
+      break;
+  }
+}
